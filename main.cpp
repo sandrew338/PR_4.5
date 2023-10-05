8752cda73d7f59e476f4d060e73176fe74dded96
sandrew338 <andrey.nazarkevych@gmail.com>
2023-10-05 12:40:38 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,29 +1,8 @@
-/*
-// Lab_04_4.cpp
+// Lab_04_5.cpp
 // < Назаркевич, Андрій>
 // Лабораторна робота № 4.5
 // Розгалуження, задане графіком функції.
 // Варіант 17
-#include <iostream>
-using namespace std;
-int main()
-{
-	double x; // вхідний аргумент
-	double y; // вхідний параметр
-	double R;
-	
-	cout << "x = "; cin >> x;
-	cout << "y = "; cin >> y;
-
-	// розгалуження в повній формі
-	if ((y <= pow(R * R - pow(x + R, 2), 2) && y >= 0 && y <= R && x >= -R && x <= 0) || (y <= pow(R * R - pow(x - R, 2), 2) && y >= -R && y <= 0 && x >= 0 && x <= R))
-		cout << "yes" << endl;
-	else
-		cout << "no" << endl;
-	cin.get();
-	return 0;
-}
-*/
 
 #include <iostream>
 #include <iomanip>
@@ -33,13 +12,14 @@ int main()
 {
 	double x, y, R, A, B;
 	srand((unsigned) time(NULL));
+	
+	cout << "A = "; cin >> A;
+	cout << "B = "; cin >> B;
 	do
 	{
 		cout << "R = ";
 		cin >> R;
 	} while (R <= 0);
-	cout << "A = "; cin >> A;
-	cout << "B = "; cin >> B;
 	for (int i = 0; i < 10; i++)
 	{
 		cout << "x = "; cin >> x;
